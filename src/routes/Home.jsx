31ff863abdee6313a46bf68765ba9ac086ff73f4
Febri Fahmi <fahmi_fafa@yahoo.com>
2023-10-13 11:16:29 +0700

    Fix: fixed getting data out from Promise and use it in views by using  useEffect and useState properly

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 export const Home = () => {
     const navigate = useNavigate();
     const landing = () => navigate("/");
-    const [articles, setArticles] = useState()
+    const [articles, setArticles] = useState();
 
     const getArticles = () => {
         const data = fetch(APIURLConfig.baseurl + APIURLConfig.articleendpoint + "all", {
@@ -28,18 +28,29 @@ export const Home = () => {
         return data
     }
 
-    getArticles().then((isi) => setArticles(isi)) // karena result dari getArticles masih berupa Promise, belum datanya, maka data perlu dibaca dan dimasukkan dulu ke useState
+    useEffect(() => {
+        getArticles()
+            .then((isi) => setArticles(isi))   // karena result dari getArticles masih berupa Promise, belum datanya, maka data perlu dibaca dan dimasukkan dulu ke useState
+            .catch((err) => console.log(err.message))
+    }, [])
+
 
     return (
         <>
             <div className="w-full">
                 <Breadcrumb />
                 <div className="flex gap-4 justify-between px-4">
-                    <div>
+                    <div className="py-10 flex gap-4">
                         {articles ? articles.articles.map((item) => (
-                            <div key={item.idarticle}>{item.articletitle}</div>
+                            <div className="flex flex-col justify-start p-5 bg-slate-900 hover:bg-black rounded-md w-1/6" key={item.idarticle}>
+                                <hr className="border-slate-700 py-2 border-dotted" />
+                                <div>{item.articletitle}</div>
+                                <div className="text-xs text-slate-700 mt-2">{item.created_at}</div>
+                            </div>
                         )) : ""}
                     </div>
+                </div>
+                <div className="flex justify-between px-5">
                     <p className="text-white">Kembali ke Landing</p>
                     <button className="text-cyan-600 font-bold" onClick={landing} >Landing</button>
                 </div>
