29a980dfae6066ff3047e138224dc4a2ab5b2ca5
Febri Fahmi <fahmi_fafa@yahoo.com>
2023-10-14 05:09:27 +0700

    Refactor: refactoring by creating NavbarItem and updating AdmNavbar and PengurusNavbar to reflect the changes

--- src/components/admin/AdmNavbar.jsx
+++ src/components/admin/AdmNavbar.jsx
@@ -1,11 +1,24 @@
+import { NavbarItem } from "../NavbarItem"
+import { useState } from "react"
+
 export const AdmNavbar = () => {
+    const [selected, setSelected] = useState();
+
     return (
         <>
             <div className="flex gap-1 text-white text-sm mt-2 mx-2">
-                <button className="bg-slate-900 hover:bg-green-700 text-green-500 hover:text-white hover:font-bold px-4 py-1 rounded-full">Dashboard</button>
-                <button className="bg-slate-900 hover:bg-green-700 text-green-500 hover:text-white hover:font-bold px-4 py-1 rounded-full">User Management</button>
-                <button className="bg-slate-900 hover:bg-green-700 text-green-500 hover:text-white hover:font-bold px-4 py-1 rounded-full">Pengaturan</button>
-                <button className="bg-slate-900 hover:bg-green-700 text-green-500 hover:text-white hover:font-bold px-4 py-1 rounded-full">Logs</button>
+                <div onClick={() => setSelected("Dashboard")}>
+                    <NavbarItem title="Dashboard" selected={selected} />
+                </div>
+                <div onClick={() => setSelected("User Management")}>
+                    <NavbarItem title="User Management" selected={selected} />
+                </div>
+                <div onClick={() => setSelected("Settings")}>
+                    <NavbarItem title="Settings" selected={selected} />
+                </div>
+                <div onClick={() => setSelected("Logs")}>
+                    <NavbarItem title="Logs" selected={selected} />
+                </div>
             </div>
         </>
     )
--- src/components/pengurus/PengurusNavbar.jsx
+++ src/components/pengurus/PengurusNavbar.jsx
@@ -1,20 +1,36 @@
+import { NavbarItem } from "../NavbarItem"
+
 export const PengurusNavbar = () => {
     return (
         <>
-        <div className="flex flex-col gap-1">
-            <div className="bg-slate-800 text-sky-500 font-bold p-2 rounded-full">Cover Story</div>
-            <div className="bg-slate-800 text-sky-500 font-bold p-2 rounded-full">Persuratan</div>
-            <div className="bg-slate-800 text-sky-500 font-bold p-2 rounded-full">Agenda</div>
-            <div className="bg-slate-800 text-sky-500 font-bold p-2 rounded-full">Artikel</div>
-            <div className="bg-slate-800 text-sky-500 font-bold p-2 rounded-full">Anggaran</div>
-            <div className="bg-slate-800 text-sky-500 font-bold p-2 rounded-full">Pengumuman</div>
-            <div className="bg-slate-800 text-sky-500 font-bold p-2 rounded-full">Kepengurusan</div>
-            <div className="bg-slate-800 text-sky-500 font-bold p-2 rounded-full">Keanggotaan</div>
-            <div className="bg-slate-800 text-sky-500 font-bold p-2 rounded-full">Ads Approval</div>
-            <div className="bg-slate-800 text-sky-500 font-bold p-2 rounded-full">Job Offers</div>
-            <div className="bg-slate-800 text-sky-500 font-bold p-2 rounded-full">Training</div>
-            <div className="bg-slate-800 text-sky-500 font-bold p-2 rounded-full">Revenue Layanan</div>
-        </div>
+            {/* <div className="flex flex-col gap-1">
+                <div className="bg-slate-800 text-sky-500 font-bold p-2 rounded-full">Cover Story</div>
+                <div className="bg-slate-800 text-sky-500 font-bold p-2 rounded-full">Persuratan</div>
+                <div className="bg-slate-800 text-sky-500 font-bold p-2 rounded-full">Agenda</div>
+                <div className="bg-slate-800 text-sky-500 font-bold p-2 rounded-full">Artikel</div>
+                <div className="bg-slate-800 text-sky-500 font-bold p-2 rounded-full">Anggaran</div>
+                <div className="bg-slate-800 text-sky-500 font-bold p-2 rounded-full">Pengumuman</div>
+                <div className="bg-slate-800 text-sky-500 font-bold p-2 rounded-full">Kepengurusan</div>
+                <div className="bg-slate-800 text-sky-500 font-bold p-2 rounded-full">Keanggotaan</div>
+                <div className="bg-slate-800 text-sky-500 font-bold p-2 rounded-full">Ads Approval</div>
+                <div className="bg-slate-800 text-sky-500 font-bold p-2 rounded-full">Job Offers</div>
+                <div className="bg-slate-800 text-sky-500 font-bold p-2 rounded-full">Training</div>
+                <div className="bg-slate-800 text-sky-500 font-bold p-2 rounded-full">Revenue Layanan</div>
+            </div> */}
+            <div className="flex gap-1 text-white text-sm mt-2 mx-2">
+                <NavbarItem title="Cover Story" />
+                <NavbarItem title="Persuratan" />
+                <NavbarItem title="Agenda" />
+                <NavbarItem title="Artikel" />
+                <NavbarItem title="Anggaran" />
+                <NavbarItem title="Pengumuman" />
+                <NavbarItem title="Kepengurusan" />
+                <NavbarItem title="Keanggotaan" />
+                <NavbarItem title="Ads Approval" />
+                <NavbarItem title="Job Offers" />
+                <NavbarItem title="Training" />
+                <NavbarItem title="Revenue Layanan" />
+            </div>
         </>
     )
 }
